b013ca63a68462bc465663af508d6eaa02e1d4d1
Dan Feeney <daniel.feeney@colorado.edu>
2017-01-17 13:31:46 -0700

    Update PCA_NMF.m

--- PCA_NMF.m
+++ PCA_NMF.m
@@ -31,6 +31,39 @@ AVG_timing = mean(Timings,3);   %Average timing synergy
 figure                          
 for c = 1:subjects
    subplot(3,1,1)
+function PCA_NMF(all,x)
+%PCA_NMF will run a PCA and NMF with a specified number of factors on time
+%series data
+%   Data for n subjects should be stored in a no. frames x features x
+%   subjects matrix. x is number of factors
+%specs = ['RtES','RlES','RGMa','RGMe','RTFL','RBF','REO','RIO','LtES','LlES','LGMa','LGMe','LTFL','LBF','LEO','LIO'];
+%   The outputs will be Timings (3D), Weights (3D), and VarExp (2D) from the PCA, which
+%   are subject specific. 
+
+features = size(all,2);
+subjects = size(all,3);
+factors = x;
+
+Weights = zeros(features,x,subjects); %Preallocate weighting matrix for PCA
+Timings = zeros(size(all,1),x,subjects);      %Preallocate timing matrix for PCA
+VarExp = zeros(features,subjects);   %Preallocate VarExp so each column represents a subject and each entry is var exp by that PC
+
+%Perform PCA and varimax rotation
+for iii = 1:subjects
+    [coeff,score,latent,tsquared,explained,mu] = pca(all(:,:,iii));           %Normal PCA
+    [coeff2,score2,latent2] = factoran(all(:,:,iii),3, 'rotate','varimax');   %Varimax rotation
+
+    %Store the data
+    Timings(:,:,iii) = score(:,1:x); %Fll this with the timings from 1:x factors
+    Weights(:,:,iii) = coeff(:,1:3); %Fill this with weights from 3 factors
+    VarExp(:,iii) = explained;       %Put the var explained by first PC in first column of VarExp
+end
+
+AVG_timing = mean(Timings,3);   %Average timing synergy
+%This will plot the time series for each subject
+figure                          
+for c = 1:subjects
+   subplot(3,1,1)
     plot(Timings(:,1,c))
     hold on
     subplot(3,1,2)
@@ -80,11 +113,18 @@ Avg_cor = mean(norm_cors,3);
 Avg_cor = Avg_cor';
 
 for dd = 1:factors
-    MakeCors(Avg_cor(:,dd))
+    MakeCors(Avg_cor(:,dd));
 end
 
-% maxes = zeros(1,3);
-% norm_cors = zeros(3,16);
+for aa = 1:3
+    subplot(3,1,aa)   
+    bar(Avg_cors(:,aa))
+    set(gca,'XTickLabel',[],'TickDir','out','Box','off')
+end
+ax = gca
+ax.XTick = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16];
+ax.XTickLabel = {'RtES','RlES','RGMA','RGMe','RTFL','RBF','REO','RIO','LtES','LlES','LGMA','LGME','LTFL','LBF','LEO','LIO'}
+
 assignin('base', 'W', W);
 assignin('base', 'Norm_cors', norm_cors);
 assignin('base', 'Avg_cors', Avg_cor);
