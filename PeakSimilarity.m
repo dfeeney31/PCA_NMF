8df17b128c6a4d9468aff4915d121ef8b86c0ffe
Dan Feeney <daniel.feeney@colorado.edu>
2017-03-14 09:48:34 -0600

    Update PeakSimilarity.m

--- PeakSimilarity.m
+++ PeakSimilarity.m
@@ -56,12 +56,70 @@ A3 = zeros(modules,modules);
 for le = 1:modules
     for jjje = 1:modules
         A3(le,jjje) = corr(W(:,3,1),W(:,jjje,le+1));
+subjects = 6;
+modules = 5;        %This is the variable that is most important. must be set to the number of desired synergies
+
+A = zeros(modules,modules);
+for l = 1:modules
+    for jjj = 1:modules
+        A(l,jjj) = corr(W(:,1,1),W(:,jjj,l+1));
+    end
+end
+
+[M, I] = max(A,[],2);
+Timing_tot1(:,1) = W(:,1,1);
+for ll = 2:modules+1
+    Timing_tot1(:,ll) = W(:,I(ll-1),ll);
+end
+AVG_1 = mean(Timing_tot1,2);
+figure(1)
+plot(AVG_1, 'LineWidth',4,'Color','k');
+hold on
+for i = 1:modules
+    plot(Timing_tot1(:,i), 'Color',[0.5 0.5 0.5])
+end
+box off
+set(gcf,'color','w')
+set(gca,'TickDir', 'out','LineWidth',4,'TickDir','out','FontSize',16);
+hold off
+
+%% Second one
+A2 = zeros(modules,modules);
+for lm = 1:modules
+    for jjjm = 1:modules
+        A2(lm,jjjm) = corr(W(:,2,1),W(:,jjjm,lm+1));
+    end
+end
+
+[M2, I2] = max(A2,[],2);
+Timing_tot2(:,1) = W(:,2,1);
+for llm = 2:modules+1
+    Timing_tot2(:,llm) = W(:,I2(llm-1),llm);
+end
+AVG_2 = mean(Timing_tot2,2);
+
+figure(2)
+plot(AVG_2, 'LineWidth',4,'Color','k');
+hold on
+for im = 1:modules
+    plot(Timing_tot2(:,im), 'Color',[0.5 0.5 0.5])
+end
+box off
+set(gcf,'color','w')
+set(gca,'TickDir', 'out','LineWidth',4,'TickDir','out','FontSize',16);
+hold off
+
+%% Third one
+A3 = zeros(modules,modules);
+for le = 1:modules
+    for jjje = 1:modules
+        A3(le,jjje) = corr(W(:,3,1),W(:,jjje,le+1));
     end
 end
 
 [M3, I3] = max(A3,[],2);
 Timing_tot3(:,1) = W(:,3,1);
-for lle = 2:6
+for lle = 2:modules+1
     Timing_tot3(:,lle) = W(:,I3(lle-1),lle);
 end
 AVG_3 = mean(Timing_tot3,2);
@@ -77,7 +135,7 @@ set(gcf,'color','w')
 set(gca,'TickDir', 'out','LineWidth',4,'TickDir','out','FontSize',16);
 hold off
 
-%% Fourth module
+% Fourth module
 A4 = zeros(modules,modules);
 for l4 = 1:modules
     for jjj4 = 1:modules
@@ -87,7 +145,7 @@ end
 
 [M4, I4] = max(A4,[],2);
 Timing_tot4(:,1) = W(:,4,1);
-for ll4 = 2:6
+for ll4 = 2:modules+1
     Timing_tot4(:,ll4) = W(:,I4(ll4-1),ll4);
 end
 AVG_4 = mean(Timing_tot4,2);
@@ -113,7 +171,7 @@ end
 
 [M5, I5] = max(A5,[],2);
 Timing_tot5(:,1) = W(:,5,1);
-for ll5 = 2:6
+for ll5 = 2:modules+1
     Timing_tot5(:,ll5) = W(:,I5(ll5-1),ll5);
 end
 AVG_5 = mean(Timing_tot5,2);
